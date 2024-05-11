cd0266b709dc974247e8edfc874cc7ea5c6ee003
Nicholas Stonecipher <nickster258@users.noreply.github.com>
2024-05-11 12:35:57 -0400

    Update ACF to 1.20

--- schemati-plugin/build.gradle.kts
+++ schemati-plugin/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     // TODO: remove dumb dependency
     implementation(project(":schemati-web"))
     implementation(kotlin("stdlib-jdk8"))
-    implementation(group = "co.aikar", name = "acf-paper", version = "0.5.0-SNAPSHOT")
+    implementation(group = "co.aikar", name = "acf-paper", version = "0.5.1-SNAPSHOT")
 
     compileOnly(group = "org.spigotmc", name = "spigot-api", version = "1.16.1-R0.1-SNAPSHOT")
     compileOnly(group = "com.sk89q.worldedit", name = "worldedit-bukkit", version = "7.2.0-SNAPSHOT")
